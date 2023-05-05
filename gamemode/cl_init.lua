af9d763906fd1171aca94bc85803f933f335253e
Twistard <Twistard13@gmail.com>
2023-05-05 12:06:50 +1000

    No Comment

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,10 @@
-print("test")
+include( "shared.lua" )
+
+function GM:Move()
+end
+
+function GM:SetupMove()
+end
+
+function GM:FinishMove()
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,1 +1,36 @@
-include("init.lua")
+GM.Name = "starwarsrp"
+GM.Author = "Twist"
+GM.Email = "N/A"
+GM.Website = "imperialgaming.io"
+DeriveGamemode("sandbox")
+
+local IncludeFiles = function(root)
+    local _, folders = file.Find(root .. "*", "LUA")
+
+    for i = 1, #folders do
+        if SERVER then
+            for k, file in SortedPairs(file.Find(root .. folders[i] .. "/sv*.lua", "LUA")) do
+                include(root .. folders[i] .. "/" .. file)
+            end
+        end
+
+        for k, file in SortedPairs(file.Find(root .. folders[i] .. "/sh*.lua", "LUA")) do
+            if SERVER then
+                AddCSLuaFile(root .. folders[i] .. "/" .. file)
+                include(root .. folders[i] .. "/" .. file)
+            else
+                include(root .. folders[i] .. "/" .. file)
+            end
+        end
+
+        for k, file in SortedPairs(file.Find(root .. folders[i] .. "/cl*.lua", "LUA")) do
+            if SERVER then
+                AddCSLuaFile(root .. folders[i] .. "/" .. file)
+            else
+                include(root .. folders[i] .. "/" .. file)
+            end
+        end
+    end
+end
+
+IncludeFiles( GM.FolderName .. "/gamemode/modules/" )
