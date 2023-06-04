07fc880261c605302bdef25cd27a82e59d1e55c9
gris-fuego <gris.fuego7@gmail.com>
2023-06-04 07:14:27 +0000

    Add placeholder nametag

--- gamemode/modules/name/module/cl_nametags.lua
+++ gamemode/modules/name/module/cl_nametags.lua
@@ -1,3 +1,46 @@
+local meta = FindMetaTable("Player")
+
+local function safeText(text)
+    return string.match(text, "^#([a-zA-Z_]+)$") and text .. " " or text
+end
+
+local function drawText(text, font, x, y, color, xAlign)
+    return draw.DrawText(safeText(text), font, x, y, color, xAlign)
+end
+
+meta.drawPlayerInfo = function(self)
+    local bone = self:LookupBone("ValveBiped.Bip01_Head1")
+    local pos
+    if bone then
+        pos = self:GetBonePosition(bone)
+    else
+        pos = self:EyePos()
+    end
+
+    pos.z = pos.z + 15
+    pos = pos:ToScreen()
+
+    local rank, nick = self:GetRankName(), self:GetName()
+    local col = IG.Regiments[self:GetRegiment()].colour
+    local reg = self:GetRegimentName()
+    local hp, maxhp = self:Health(), self:GetMaxHealth()
+
+    drawText(rank .. " " .. nick, "TargetID", pos.x, pos.y + 1, col, 1)
+    drawText(reg, "TargetID", pos.x, pos.y + 21, col, 1)
+    drawText(string.format("%d%%", hp / maxhp * 100), "TargetID", pos.x, pos.y + 41, col, 1)
+end
+
 function GM:HUDDrawTargetID()
-    
+    local tr = util.GetPlayerTrace( LocalPlayer() )
+	local trace = util.TraceLine( tr )
+	if ( !trace.Hit ) then return end
+	if ( !trace.HitNonWorld ) then return end
+
+    if ( trace.Entity:IsPlayer() ) then
+        if trace.HitPos:DistToSqr(trace.StartPos) < 20000 then
+            if !trace.Entity:ShouldHideName() then
+                trace.Entity:drawPlayerInfo()
+            end
+        end
+	end
 end
