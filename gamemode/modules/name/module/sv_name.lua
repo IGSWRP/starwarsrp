5e9b85d0b26f40329e3cbe8f461df1a63e3a5b0a
gris-fuego <gris.fuego7@gmail.com>
2023-06-04 06:26:23 +0000

    Add setname command

--- gamemode/modules/name/module/sv_name.lua
+++ gamemode/modules/name/module/sv_name.lua
@@ -3,5 +3,40 @@ local meta = FindMetaTable("Player")
 function meta:SetPlayerName(name)
     self:SetRPName(name)
 
-    -- TODO: database
+    player_manager.RunClass(self, "SaveCharacterData")
 end
+
+local sub = string.sub
+
+hook.Add("PlayerSay", "IG.ChangeName", function(ply, text)
+    prefix = sub(text, 1, 1)
+    if prefix ~= "/" then return end
+    if string.match(sub(text, 2, 2), "[a-z]") == nil then return end
+
+    local cmd = sub(text, 2)
+    local text
+
+    if string.StartsWith(cmd, "setname") then
+        text = sub(cmd, #"setname" + 2)
+    elseif string.StartsWith(cmd, "name") then
+        text = sub(cmd, #"name" + 2)
+    else
+        return 
+    end
+
+    print(text)
+    
+    if string.len(text) < 2 then
+        ply:ChatPrint("That name is too short")
+        return ""
+    elseif string.len(text) > 32 then
+        ply:ChatPrint("That name is too long")
+
+        return ""
+    end
+
+    ply:SetPlayerName(text)
+
+    ply:ChatPrint("Set name to " .. text)
+    return ""
+end)
