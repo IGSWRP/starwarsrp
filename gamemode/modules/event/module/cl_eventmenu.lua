8d72b7ced44a7aaa43fb4abe4edb35f32d5d71a2
gris-fuego <gris.fuego7@gmail.com>
2023-06-06 13:47:27 +0000

    Add weapon tab to preset editor

--- gamemode/modules/event/module/cl_eventmenu.lua
+++ gamemode/modules/event/module/cl_eventmenu.lua
@@ -1,7 +1,7 @@
 local function EditPreset(preset_name)
     local frame = vgui.Create("DFrame")
-    frame:SetSize(720, 480)
-    frame:SetTitle("Edit preset")
+    frame:SetSize(720, 510)
+    frame:SetTitle((preset_name and "Edit") or "Add" .. " preset")
     frame:SetPos(gui.MousePos())
     frame:SetVisible(true)
     frame:MakePopup()
@@ -76,9 +76,68 @@ local function EditPreset(preset_name)
 
     local weapon_panel = vgui.Create("DPanel", sheet)
 
+    local weapon_list = vgui.Create("DListView", weapon_panel)
+    weapon_list:Dock(LEFT)
+    weapon_list:SetSize(280, 0)
+    weapon_list:SetMultiSelect(false)
+    weapon_list:AddColumn("Weapon")
+    
+    local weapon_picker = vgui.Create("DPanel", weapon_panel)
+    weapon_picker:Dock(RIGHT)
+    weapon_picker:SetSize(400, 0)
+
+    local weapon_search = vgui.Create("DTextEntry", weapon_picker)
+    weapon_search:Dock(TOP)
+    weapon_search:DockMargin(0, 0, 0, 5)
+    weapon_search:SetPlaceholderText( "#spawnmenu.quick_filter" )
+    weapon_search:SetUpdateOnType(true)
+    
+    local weapon_scroll = vgui.Create("DScrollPanel", weapon_picker)
+    weapon_scroll:Dock( FILL )
+
+    local weapon_icons = vgui.Create("DIconLayout", weapon_scroll)
+    weapon_icons:Dock(FILL)
+    
+    for _, weapon in pairs(weapons.GetList()) do
+        if string.EndsWith(weapon.ClassName, "_base") then continue end
+        local icon = weapon_icons:Add("DImageButton")
+        icon:SetImage(string.format("materials/entities/%s.png", weapon.ClassName))
+        icon:SetSize(96,96)
+        icon:SetTooltip(weapon.PrintName)
+        icon.playerweapon = weapon.PrintName or ""
+        icon.weapon_path = weapon.ClassName
+        icon.DoClick = function()
+            local weapon_line = weapon_list:AddLine(weapon.ClassName)
+            weapon_line.OnRightClick = function()
+                weapon_list:RemoveLine(weapon_line:GetID())
+            end
+        end
+    end
+
+    weapon_search.OnValueChange = function(s, str)
+        for id, pnl in pairs(weapon_icons:GetChildren()) do
+            if str ~= "" and ( !pnl.playerweapon:find( str, 1, true ) && !pnl.weapon_path:find( str, 1, true ) ) then
+                pnl:SetVisible(false)
+            else
+                pnl:SetVisible(true)
+            end
+        end
+        weapon_icons:Layout()
+    end
+
     sheet:AddSheet("Details", detail_panel, "icon16/information.png")
     sheet:AddSheet("Models", model_panel, "icon16/user_suit.png")
     sheet:AddSheet("Weapons", weapon_panel, "icon16/gun.png")
+
+    local bottombar = vgui.Create("DPanel", frame)
+    bottombar:SetSize(0, 25)
+    bottombar:Dock(BOTTOM)
+    bottombar:DockMargin(0,5,0,0)
+
+    local submit_button = vgui.Create("DButton", bottombar)
+    submit_button:SetSize(100, 0)
+    submit_button:SetText("Submit")
+    submit_button:Dock(RIGHT)
 end
 
 local function EventMenu()
@@ -122,6 +181,15 @@ local function EventMenu()
     preset_edit:Dock(LEFT)
     preset_edit.DoClick = function()
         EditPreset("test")
+    end
+
+    local preset_add = vgui.Create("DButton", topbar)
+    preset_add:SetSize(25, 0)
+    preset_add:SetText("")
+    preset_add:SetImage("icon16/database_add.png")
+    preset_add:Dock(LEFT)
+    preset_add.DoClick = function()
+        EditPreset()
     end
 
     local invite_button = vgui.Create("DButton", topbar)
